ce97c801e55c6eceb3c75587ed10119317fa7e68
Cringe <jackjack98989@gmail.com>
2020-08-24 17:55:13 +1000

    Resolution

---
+++ Assets/scripts/OptionsMenu.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsMenu : MonoBehaviour
+{
+
+    public Resolution[] resolutions;
+    public Dropdown resolution;
+
+    void Start()
+    {
+        resolutions = Screen.resolutions;
+        resolution.ClearOptions();
+        List<string> options = new List<string>();
+
+
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)//go through every resolution
+        {
+            //build a string for displaying the resolution
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                //Found current resolution, save it
+                currentResolutionIndex = i;
+            }
+        }
+
+        //set up the dropdown
+        resolution.AddOptions(options);
+        resolution.value = currentResolutionIndex;
+        resolution.RefreshShownValue();
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution res = resolutions[resolutionIndex];
+        Screen.SetResolution(res.width, res.height, false);
+    }
+}
