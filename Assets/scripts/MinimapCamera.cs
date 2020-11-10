a9c4c7932c5aaf8fbde3657e4e17c9c856f8e10f
CringeWorthyStudios <42565124+CringeWorthyStudios@users.noreply.github.com>
2020-11-10 16:06:21 +1100

    Minimap and Normal PC push

---
+++ Assets/scripts/MinimapCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapCamera : MonoBehaviour
+{
+    public GameObject player;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z);
+        transform.eulerAngles = new Vector3(90, 0, player.transform.eulerAngles.z);
+    }
+}
